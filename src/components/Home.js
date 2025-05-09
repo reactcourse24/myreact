a55f1e6c7b8f733fbf797c29300ee62b987db204
Code with Channi <133969778+reactcourse24@users.noreply.github.com>
2025-05-09 15:56:16 +0530

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Home() {
   return (
     <div>
-      <h1 className="text-center my-5">Welcome to react HOME!<br/> I love React</h1>
+      <h1 className="text-center my-5">Welcome to react HOME!<br/> I love to learn React</h1>
     </div>
   )
 }
